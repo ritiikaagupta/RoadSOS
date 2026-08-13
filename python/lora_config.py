c0eee2753ac92c77b452e4eb1914de4b547991e5
Ritika Gupta <gritika950@gmail.com>
2026-08-13 22:27:12 +0530

    Create lora_config.py

---
+++ python/lora_config.py
@@ -0,0 +1,13 @@
+
+# SAFE-Link LoRa Configuration
+
+# Temporary values.
+# These will be finalized after the hardware
+# and RF architecture are fixed.
+
+SPREADING_FACTOR = 7
+BANDWIDTH_HZ = 125_000
+
+# Temporary sampling rate.
+# This is NOT the final hardware sampling rate.
+SAMPLE_RATE_HZ = 1_000_000
